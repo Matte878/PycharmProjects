638f8dc402acf9c1f8c1ac466947ff597f33614c
MC <81518660+MatteoChendi@users.noreply.github.com>
2022-03-27 12:18:04 +0200

    Update Memory_final_game_v1.py

--- Memory_final_game_v1.py
+++ Memory_final_game_v1.py
@@ -1,3 +1,7 @@
+# This project is about the classical Memory game, where you need to find and match pairs of cards. 
+# Here the pictures used for the cards will change randomly every new game you play. They are chosen from a list of pictures taken via WhatsApp. 
+# In order for you to make the code work you should have pictures which name starts with "Sreenshot" and move them into the same folder where you python file is located.
+
 import sys
 import pygame
 import os
