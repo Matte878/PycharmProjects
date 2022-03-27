8d554306696bf391a6f5e8db25123b3c77fd5cdf
MC <81518660+MatteoChendi@users.noreply.github.com>
2022-03-27 12:26:56 +0200

    Update Claim_analysis.py

--- Claim_analysis.py
+++ Claim_analysis.py
@@ -1,3 +1,6 @@
+# By using pandas, a representation on a bar chart of age of claims occured (early, middle or late claims) based on segmented age of policyholders.
+# Data are elaborated starting from a raw database.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
